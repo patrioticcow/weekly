045c504efa89fc8e1cb827f2ab3629b3644f4b30
patrioticcow <cristi.catea@gmail.com>
2016-02-18 12:39:12 -0800

    updates

---
+++ app/pages/tutorial/tutorial.js
@@ -0,0 +1,48 @@
+import {IonicApp, Page, NavController, MenuController} from 'ionic/ionic';
+import {TabsPage} from '../tabs/tabs';
+import {LoginPage} from '../login/login';
+
+@Page({
+  templateUrl: 'build/pages/tutorial/tutorial.html'
+})
+export class TutorialPage {
+  constructor(app: IonicApp, nav: NavController, menu: MenuController) {
+    this.app = app;
+    this.nav = nav;
+    this.menu = menu;
+
+    this.slides = [
+      {
+        title: "Welcome to <b>ICA</b>",
+        description: "The <b>Ionic Conference App</b> is a practical preview of the Ionic Framework in action, and a demonstration of proper code use.",
+        image: "img/ica-slidebox-img-1.png",
+      },
+      {
+        title: "What is Ionic?",
+        description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+        image: "img/ica-slidebox-img-2.png",
+      },
+      {
+        title: " What is Ionic Platform?",
+        description: "The <b>Ionic Platform</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+        image: "img/ica-slidebox-img-3.png",
+      }
+    ];
+  }
+
+  startApp() {
+    let nav = this.app.getComponent('nav');
+    nav.setRoot(LoginPage);
+  }
+
+  onPageDidEnter() {
+    // the left menu should be disabled on the tutorial page
+    this.menu.enable(false);
+  }
+
+  onPageDidLeave() {
+    // enable the left menu when leaving the tutorial page
+    this.menu.enable(true);
+  }
+
+}
